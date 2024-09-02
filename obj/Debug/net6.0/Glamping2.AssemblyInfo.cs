776b9069d9d2e3df6439ec8fb84bf3521726c6ed
Leorojas96 <leorojast96@gmail.com>
2024-09-02 18:03:21 -0500

    duplicidad borrada

--- obj/Debug/net6.0/Glamping2.AssemblyInfo.cs
+++ obj/Debug/net6.0/Glamping2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Glamping2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce152336cff1656a30d2aa6f8d3f6c7d7d72f120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+291b102816f1360aefea251b4a3bf403407db229")]
 [assembly: System.Reflection.AssemblyProductAttribute("Glamping2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Glamping2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
